96ff4d75fda1f698457a963a3028b59073092c42
Clint Walker <clint@neonrelic.com>
2014-11-07 00:34:58 +0200

    Block commit - Qe 0.6.0 spec
    - Support .populate, .limit, .offset, .select in “find”
    - Update now applies to multiple .ids

--- index.js
+++ index.js
@@ -41,16 +41,16 @@
 
     if (!cb) throw new Error('Missing callback');
 
-    if (!query.action || !query.resource)
+    if (!query.do || !query.on)
       return cb('Invalid query: must provide `action` and `resource`');
 
-    if (memory[ query.action ]) return memory[ query.action ]( query, cb );
+    if (memory[ query.do ]) return memory[ query.do ]( query, cb );
     else cb && cb( 'No matching action' );
   };
 
 
   /**
-    Internal helper: finds a record from table `resource` by `id`
+    Helper: finds a record from table `resource` by `id`
 
     @param {String} resource
     @param {String} id
@@ -58,14 +58,79 @@
     @private
   */
 
-  function _find( resource, id ) {
-    var _ret;
+  function _find( resource, ids ) {
+    var _ret = [];
 
-    memory._store[ resource ].forEach( function (rec, i) {
-      if (rec.id === id) _ret = {index:i, record:rec};
+    if (!(ids instanceof Array)) ids = [ids];
+
+    ids.forEach( function (id) {
+      memory._store[ resource ].forEach( function (rec, i) {
+        if (rec.id === id) _ret.push({index:i, record:rec});
+      });
     });
 
     return _ret;
+  }
+
+
+  /**
+    Helper: Cheap/nasty async checker
+    Setup a count, have your code decrement the count when passing to chkdone
+  */
+
+  function _chkdone (count, cb, res) {
+    if (count === 0) cb( null, res );
+  };
+
+
+  /**
+    Helper: Grabs the lastKey from an object. Or first key if only one key.
+    10x faster than Object.keys. I know. Who cares in this adapter.
+    http://jsperf.com/unknown-object-key
+  */
+
+  function _lastkey (block) {
+    var ret;
+    for (var key in block) ret = key;
+    return ret;
+  }
+
+
+  /**
+    Helper: Squashes a collection of objs to an array of values keyed on 'key'
+  */
+
+  function _squash (objs, key) {
+    var ret = [];
+    objs.forEach( function (o) {
+      ret.push(o[key]);
+    });
+    return ret;
+  }
+
+
+  /**
+    Helper: Whitelist/blacklist fields in elements 'res'
+    Ugly/slow/nasty
+  */
+
+  function _select (res, sel) {
+    if (!sel) return res;
+
+    res.forEach( function (r) {
+      var keys = Object.keys(r);
+      var remove = [];
+
+      sel.forEach( function (s,i) {
+        if (s[0] === '-') remove[i] = s.slice(1);
+      });
+
+      if (remove.length) remove.forEach( function (s) { delete r[s]; });
+      else keys.forEach( function (k) {
+        if ( sel.indexOf(k) < 0 ) delete r[k];
+      });
+    });
+    return res;
   }
 
 
@@ -73,13 +138,13 @@
   /**
     Creates new record/s
 
-    @param {QueryObject} qo
+    @param {Qe} qe
     @param {Function} cb
 
     @private
   */
 
-  memory.create = function( qo, cb ) {
+  memory.create = function( qe, cb ) {
     var created = [];
 
     var insert = function (record) {
@@ -88,69 +153,53 @@
       var id = Math.random().toString(36).substr(2);
       record.id = id;
 
-      if (!memory._store[ qo.resource ]) memory._store[ qo.resource ] = [];
-      memory._store[ qo.resource ].push( record );
+      if (!memory._store[ qe.on ]) memory._store[ qe.on ] = [];
+      memory._store[ qe.on ].push( record );
       created.push( record );
     };
 
-    qo.body.forEach( insert );
+    qe.body.forEach( insert );
 
     if (cb) cb( null, created.length > 1 ? created : created[0] );
   };
 
 
   /**
-    Save (PUT) a complete record/s
+    Update (partial) a record/s
 
-    @param {QueryObject} qo
+    @param {QueryObject} qe
     @param {Function} cb
 
     @private
   */
 
-  memory.save = function( qo, cb ) {
+  memory.update = function( qe, cb ) {
 
-    qo.body.forEach( function (update) {
-      if (!update.id) cb('Must provide records with `id` set');
-
-      var found = _find( qo.resource, update.id );
-      // @note Could update this to support updateOrCreate
-      if (!found)
-        cb('Cannot find record to update: '+qo.resource+'.'+update.id);
-
-      memory._store[ qo.resource ][ found.index ] = update;
-    });
-
-    return cb( null, qo.body );
-  };
-
-
-  /**
-    Update (partial PATCH) a record/s
-
-    @param {QueryObject} qo
-    @param {Function} cb
-
-    @private
-  */
-
-  memory.update = function( qo, cb ) {
-
-    if (!qo.body || !qo.body.length)
-      return cb('Must provide updates in Query .body');
-
-    if (!qo.ids || !qo.ids.length)
+    if (!qe.ids || !qe.ids.length)
       return cb('Must provide ids to update in Query .ids field');
 
-    // This implementation ONLY updates the first update request
-    var found = _find( qo.resource, qo.ids[0] );
+    var found = _find( qe.on, qe.ids );
+    // @todo Is "not found" supposed to return in this error channel?
     if (!found) return cb('Record not found to update');
 
-    for (var key in qo.body[0]) {
-      memory._store[ qo.resource ][ found.index ][ key ] = qo.body[0][ key ];
-    }
 
-    return cb( null, memory._store[ qo.resource ][ found.index ] );
+    var db = memory._store[ qe.on ];
+
+    found.forEach( function (res) {
+
+      var dbrec = db[ res.index ];
+
+      // Apply provided body updates (ie. partial update, no delete)
+      if (qe.body) {
+        for (var key in qe.body[0]) {
+          dbrec[ key ] = qe.body[0][ key ];
+        }
+      }
+
+    });
+
+
+    return cb( null, _squash(found, 'record') );
   };
 
 
@@ -158,14 +207,14 @@
     Deletes a record/s
   */
 
-  memory.remove = function( qo, cb ) {
+  memory.remove = function( qe, cb ) {
 
-    if (!qo.ids || !qo.ids.length)
+    if (!qe.ids || !qe.ids.length)
       return cb( 'Must provide ids to remove in Query .ids' );
 
-    qo.ids.forEach( function(id) {
-      var found = _find( qo.resource, id );
-      if (found) memory._store[ qo.resource ].splice( found.index, 1 );
+    qe.ids.forEach( function(id) {
+      var found = _find( qe.on, id );
+      if (found) memory._store[ qe.on ].splice( found.index, 1 );
     });
 
     return cb( null, true );
@@ -175,31 +224,64 @@
   /**
     Retrieve record/s
 
-    @param {QueryObject} qo
+    @param {QueryObject} qe
     @param {Function} cb
 
     @private
   */
 
-  memory.find = function( qo, cb ) {
+  memory.find = function( qe, cb ) {
     var found = [];
 
-    // Find MANY
-    if (!qo.ids || !qo.ids.length) {
-      // @todo Handle conditions
-
-      if (memory._store[ qo.resource ])
-        found = memory._store[ qo.resource ].slice(0,100);
-
-      return cb( null, found );
+    // Specifically Get by IDs
+    if (qe.ids) {
+      _find( qe.on, qe.ids ).forEach( function (rec) {
+        found.push( rec.record );
+      })
     }
-    else {
-      qo.ids.forEach( function(id) {
-        var rec = _find( qo.resource, id );
-        if (rec) found.push( rec.record );
+    // Otherwise: find MANY
+    else if (memory._store[ qe.on ])
+      found = memory._store[ qe.on ];
+
+    // Clone the results so we're not destroying the DB
+    // omfg this is nasty. Serious eye bleeding. Never production.
+    found = JSON.parse( JSON.stringify(found) );
+
+    // Offset
+    if ('number' === typeof qe.offset ) found = found.slice(qe.offset);
+
+    // Limit results
+    if (qe.limit) found = found.slice(0, qe.limit);
+
+    // Apply selection
+    found = _select( found, qe.select );
+
+    // Populate
+    if (qe.populate) {
+      // Cheap/nasty async: number of searches required
+      var _as = found.length * Object.keys(qe.populate).length;
+
+      Object.keys( qe.populate ).forEach( function (key) {
+        var pop = qe.populate[ key ];
+
+        // Ugh looking up every record is DUMB
+        found.forEach( function (res) {
+          if (pop.query) pop.query.ids = res[key];
+
+          var nq = pop.query || {on:key, ids:res[key]};
+
+          memory.find( nq, function (e,r) {
+            if (e) return cb('Died: '+e);
+            res[key] = r;
+            _chkdone( --_as, cb, found );
+          });
+        });
       });
-      return cb( null, found );
     }
+
+    else return cb( null, found );
+
+
   };
 
 
